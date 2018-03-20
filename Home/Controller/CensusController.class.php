1939fd2f70acda6cee10df9e54859a9ed5866c3e
djhj2008 <djhj_2001@126.com>
2018-03-20 14:33:52 +0800

    add login reg

---
+++ Home/Controller/CensusController.class.php
@@ -0,0 +1,40 @@
+<?php
+namespace Home\Controller;
+use Think\Controller;
+class CensusController extends Controller {
+    public function saleinfo(){
+
+    }
+
+    public function regsale(){
+        if(!empty($_POST)){
+            $name=$_POST['name'];
+            $phone=$_POST['number'];
+            $info=$_POST['info'];
+            $phone2=$_POST['number2'];
+
+            $ret = M('lp_census')->where(array('phone'=>$phone ))
+                ->find();
+
+            if(!empty($ret)){
+                Alert("手机已被注册.",NULL,"Census/regsale");
+                exit;
+            }
+            $user = array();
+            $user['name']=$name;
+            $user['group_num']=$info;
+            $user['phone']=$phone;
+            $user['levl_group']=$phone2;
+            $ret3 = M('lp_census')->add($user);
+            if(empty($ret3)){
+                Alert("注册失败.",NULL,"Census/regsale");
+                exit;
+            }else{
+                echo "<script language=\"JavaScript\">\r\n";
+                echo " alert(\"注册成功.\");\r\n";
+                echo "</script>";
+            }
+        }
+        $this->display();
+    }
+}
